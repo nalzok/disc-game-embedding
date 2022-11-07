24adf9465f142f3bba924b12d2d259c0e8da6d22
Qingyao Sun <sunqingyao19970825@icloud.com>
2022-11-07 09:18:01 -0600

    fix embedding ordering

--- scripts/embed.py
+++ scripts/embed.py
@@ -11,28 +11,37 @@ def embed(F):
     diagT = np.diagonal(T, offset=-1)
     even = np.arange(0, n if n % 2 == 0 else n - 1, 2)
     odd = np.arange(1, n, 2)
+    reorder = np.arange(n)
+
+    # Make sure each 2x2 block have sign [[0, +], [-, 0]]
+    reorder[even] += diagT[even] > 0
+    reorder[odd] -= diagT[even] > 0
+    Q = Q[:, reorder]
+
     diagSqrtD = np.zeros(n)
     diagSqrtD[even] = diagSqrtD[odd] = np.sqrt(np.abs(diagT[even]))
     sqrtD = np.diag(diagSqrtD)
 
     # Sanity check
     R = np.zeros((n, n))
-    sign = np.sign(diagT[even])
-    R[even, odd] = -sign
-    R[odd, even] = sign
+    R[even, odd] = 1
+    R[odd, even] = -1
     np.testing.assert_allclose(F, Q @ sqrtD @ R @ sqrtD @ Q.T, atol=0.001)
 
     embedding = sqrtD @ Q.T
-    return embedding
+    eigen = np.diag(sqrtD)**2
+    return embedding, eigen
 
 
 @click.command()
 @click.option("--payoff", type=click.Path(path_type=Path), required=True)
 @click.option("--embedding", type=click.Path(path_type=Path), required=True)
-def cli(payoff: Path, embedding: Path):
+@click.option("--eigen", type=click.Path(path_type=Path), required=True)
+def cli(payoff: Path, embedding: Path, eigen: Path):
     F = np.load(payoff)
-    ebd = embed(F)
-    np.save(embedding, ebd)
+    embedding_, eig = embed(F)
+    np.save(embedding, embedding_.T)
+    np.save(eigen, eig)
 
 
 if __name__ == "__main__":
